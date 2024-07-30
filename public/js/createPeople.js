17f57e3ed70de8ca1c33c8e74256b1eead9d97c7
Ethan Henry Carpenter <carpenterhethan@gmail.com>
2024-07-31 00:33:17 +0100

    can now create people on demand and got the styling from gpt in fact all the styles are from gpt im not that creative, why i prefer command line apps and algs no front end required

--- public/js/createPeople.js
+++ public/js/createPeople.js
@@ -1,5 +1,31 @@
-function createPerson(name){
+function createPerson(firstname,surname){
     const outerDiv=document.createElement("div");
     outerDiv.setAttribute("class","person-item");
-    outerDiv.setAttribute("id",name);
+    outerDiv.setAttribute("id",firstname+" "+surname);
+    const info=document.createElement("div");
+    info.setAttribute("class","person-info");
+    const personNameSpan=document.createElement("span");
+    personNameSpan.setAttribute("class","person-name");
+    personNameSpan.innerText=surname+", "+firstname;
+    const newTestButton=document.createElement("button");
+    newTestButton.setAttribute("class","add-test-button");
+    newTestButton.setAttribute("id",firstname+" "+surname+" Button");
+    newTestButton.innerText="+ Test"
+    const testList=document.createElement("div");
+    testList.setAttribute("class","test-list")
+
+    info.appendChild(personNameSpan);
+    info.appendChild(newTestButton);
+    outerDiv.appendChild(info);
+    outerDiv.appendChild(testList);
+
+    const peopleContainer=document.getElementsByClassName("people-container")
+    peopleContainer[0].appendChild(outerDiv);
 }
+
+createPerson("John","Doe")
+createPerson("John","Doe")
+createPerson("John","Doe")
+createPerson("John","Doe")
+createPerson("John","Doe")
+createPerson("John","Doe")
